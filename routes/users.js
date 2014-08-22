27db37677af0647e7e10c757ece4993c92077c48
mishra <tech@peoplevendor.com>
2014-08-22 12:35:12 -0700

    asdf

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var router = express.Router();
+var uuid = require('node-uuid');
 
 var arr_users = [];
-var sessions = [];
+var sessions = {};
 
 function checkExisting(req, res, next){
     // this is a middleware, checks for existing user
@@ -17,26 +18,36 @@ function checkExisting(req, res, next){
     }
 }
 
+function authorize(req, res, next){
+    if (typeof req.cookies.sessionid !== 'undefined'  &&
+        req.cookies.sessionid in sessions) {
+        req.user = arr_users[sessions[req.cookies.sessionid].userid];
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
 router.post('/', checkExisting, function (req, res) {
     arr_users.push(req.body);
     // create a session for the user
-    sessions.push({
+    sessions[uuid.v4()] = {
         userid: arr_users.length - 1,
         timestamp: new Date()
-    });
+    };
     res.cookie('sessionid', sessions.length - 1);
     res.json(200, { name: req.body.username});
+    console.log(sessions);
 });
 
-router.get('/account', function (req, res){
-    console.log(typeof req.cookies.sessionid);
-    var sessionid = Number(req.cookies.sessionid);
-    console.log(typeof sessionid);
-    // get the session
-    // get the userid from the session
-    // send the details back to the page
-    res.render('accounts', arr_users[sessions[sessionid].userid]);
+router.get('/account', authorize, function (req, res){
 
+    res.render('accounts', req.user);
+
+});
+
+router.get('/sensitive', authorize, function (){
+    res.json(200, 'This data is only visible to authenticated users ... ');
 });
 
 module.exports = router;
